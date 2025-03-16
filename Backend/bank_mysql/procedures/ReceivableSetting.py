fdc9599e3a91627ab004368e462fc26a0e9d57f2
minwoo kim <minu0897@naver.com>
2025-03-16 15:11:03 +0900

    modify procedure

--- Backend/bank_mysql/procedures/ReceivableSetting.py
+++ Backend/bank_mysql/procedures/ReceivableSetting.py
@@ -1,22 +1,25 @@
-from ..db import mysql_connection
+from ..db.mysql_connection import get_db_connection
 
-def receivablesetting_mysql(remittance_id, receivable_id,amount):
-    #송금 준비 프로시저
+def receivablesetting_mysql(remittance_id, receivable_id, amount):
+    # 송금 준비 프로시저
 
-    #remittance_id : 송금자 id
-    #receivable_id : 수금자 id
-    #amount        : 입금 금액
-    conn = mysql_connection()
+    # remittance_id : 송금자 id
+    # receivable_id : 수금자 id
+    # amount        : 입금 금액
+    conn = get_db_connection()
     cursor = conn.cursor()
+    
     try:
-        cursor.callproc("RemittanceSetting", (remittance_id, receivable_id,amount, "@status"))
-        cursor.execute("SELECT @status;")
-        status = cursor.fetchone()["@status"]
+        # 프로시저 호출 (OUT 파라미터 없이)
+        cursor.execute("CALL ReceivableSetting(%s, %s, %s)", (remittance_id, receivable_id, amount))
+        # 반환된 결과 집합 가져오기
+        result = cursor.fetchone()  # { "create_id": 23, "status": "SUCCESS" } 형태
         conn.commit()
-        return status
+        print(result)
+        return [result["create_id"], result["status"]]
     except Exception as e:
         conn.rollback()
-        return str(e)
+        return [str(e), None]
     finally:
         cursor.close()
         conn.close()
--- Backend/bank_mysql/procedures/RemittanceSetting.py
+++ Backend/bank_mysql/procedures/RemittanceSetting.py
@@ -1,4 +1,4 @@
-from ..db import mysql_connection
+from ..db.mysql_connection import get_db_connection
 
 def remittancesetting_mysql(remittance_id, receivable_id,amount):
     #수금 준비 프로시저
@@ -6,17 +6,19 @@ def remittancesetting_mysql(remittance_id, receivable_id,amount):
     #remittance_id : 송금자 id
     #receivable_id : 수금자 id
     #amount        : 입금 금액
-    conn = mysql_connection()
+    conn = get_db_connection()
     cursor = conn.cursor()
     try:
-        cursor.callproc("ReceivableSetting", (remittance_id, receivable_id,amount, "@status"))
-        cursor.execute("SELECT @status;")
-        status = cursor.fetchone()["@status"]
+        # 프로시저 호출 (OUT 파라미터 없이)
+        cursor.execute("CALL RemittanceSetting(%s, %s, %s)", (remittance_id, receivable_id, amount))
+        # 반환된 결과 집합 가져오기
+        result = cursor.fetchone()  # { "create_id": 23, "status": "SUCCESS" } 형태
         conn.commit()
-        return status
+        print(result)
+        return [result["create_id"], result["status"]]
     except Exception as e:
         conn.rollback()
-        return str(e)
+        return [str(e), None]
     finally:
         cursor.close()
         conn.close()
