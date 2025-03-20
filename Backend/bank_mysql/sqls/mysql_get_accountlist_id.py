1d2bc774187eb632eaadb5879b74372ad78e8403
minwoo kim <minu0897@naver.com>
2025-03-20 19:56:58 +0900

    add blank

--- Backend/bank_mysql/sqls/mysql_get_accountlist_id.py
+++ Backend/bank_mysql/sqls/mysql_get_accountlist_id.py
@@ -7,11 +7,11 @@ def mysql_get_accountlist_id(acc_id):
     conn = get_db_connection()
     cursor = conn.cursor()
 
-    query = "SELECT id,sender_id,receiver_id,(-1)*amount,transaction_type,transaction_date,status,result_balance"
-    query +="FROM transfer_list where sender_id = %s and status = 2 and transaction_type = 1"
-    query +="union all"
-    query +="SELECT id,sender_id,receiver_id,amount,transaction_type,transaction_date,status,result_balance"
-    query +="FROM transfer_list where receiver_id = %s and status = 2 and transaction_type = 2"
+    query = "SELECT id,sender_id,receiver_id,(-1)*amount,transaction_type,transaction_date,status,result_balance "
+    query +="FROM transfer_list where sender_id = %s and status = 2 and transaction_type = 1 "
+    query +="union all "
+    query +="SELECT id,sender_id,receiver_id,amount,transaction_type,transaction_date,status,result_balance "
+    query +="FROM transfer_list where receiver_id = %s and status = 2 and transaction_type = 2 "
     query +="order by transaction_date desc;"
 
     cursor.execute(query,(acc_id,acc_id))  # 안전한 방식 (SQL Injection 방지)
