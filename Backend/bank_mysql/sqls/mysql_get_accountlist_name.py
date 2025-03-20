c7bf1d49c3b64f99f8df1e0e067d14471bc22308
minwoo kim <minu0897@naver.com>
2025-03-20 19:56:58 +0900

    add blank

--- Backend/bank_mysql/sqls/mysql_get_accountlist_name.py
+++ Backend/bank_mysql/sqls/mysql_get_accountlist_name.py
@@ -9,11 +9,11 @@ def mysql_get_accountlist_name(name):
     conn = get_db_connection()
     cursor = conn.cursor()
 
-    query = "SELECT id,sender_id,receiver_id,(-1)*amount,transaction_type,transaction_date,status,result_balance"
-    query +="FROM transfer_list where sender_id = (select name from accounts where name = %s) and status = 2 and transaction_type = 1"
-    query +="union all"
-    query +="SELECT id,sender_id,receiver_id,amount,transaction_type,transaction_date,status,result_balance"
-    query +="FROM transfer_list where receiver_id = (select name from accounts where name = %s) and status = 2 and transaction_type = 2"
+    query = "SELECT id,sender_id,receiver_id,(-1)*amount,transaction_type,transaction_date,status,result_balance "
+    query +="FROM transfer_list where sender_id = (select name from accounts where name = %s) and status = 2 and transaction_type = 1 "
+    query +="union all "
+    query +="SELECT id,sender_id,receiver_id,amount,transaction_type,transaction_date,status,result_balance "
+    query +="FROM transfer_list where receiver_id = (select name from accounts where name = %s) and status = 2 and transaction_type = 2 "
     query +="order by transaction_date desc;"
 
     cursor.execute(query,(name,name))  # 안전한 방식 (SQL Injection 방지)
