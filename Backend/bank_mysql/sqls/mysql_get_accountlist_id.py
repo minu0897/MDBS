06667ae1102b1a61661c7e1d195baeb5a9af31a2
minwoo kim <minu0897@naver.com>
2025-03-20 19:54:34 +0900

    sql

--- Backend/bank_mysql/sqls/mysql_get_accountlist_id.py
+++ Backend/bank_mysql/sqls/mysql_get_accountlist_id.py
@@ -7,8 +7,14 @@ def mysql_get_accountlist_id(acc_id):
     conn = get_db_connection()
     cursor = conn.cursor()
 
-    query = "SELECT id,sender_id,receiver_id,amount,tranfer_type,transaction_date,status FROM transfer_list where sender_id like %s or receiver_id like %s order by id desc"
-    cursor.execute(query,(acc_id,))  # 안전한 방식 (SQL Injection 방지)
+    query = "SELECT id,sender_id,receiver_id,(-1)*amount,transaction_type,transaction_date,status,result_balance"
+    query +="FROM transfer_list where sender_id = %s and status = 2 and transaction_type = 1"
+    query +="union all"
+    query +="SELECT id,sender_id,receiver_id,amount,transaction_type,transaction_date,status,result_balance"
+    query +="FROM transfer_list where receiver_id = %s and status = 2 and transaction_type = 2"
+    query +="order by transaction_date desc;"
+
+    cursor.execute(query,(acc_id,acc_id))  # 안전한 방식 (SQL Injection 방지)
     user = cursor.fetchall()
 
     cursor.close()
--- Backend/bank_mysql/sqls/mysql_get_accountlist_name.py
+++ Backend/bank_mysql/sqls/mysql_get_accountlist_name.py
@@ -5,12 +5,18 @@ from ..db.mysql_connection import get_db_connection
 # GET
 # parms :
 #        - name : 이름
-def mysql_get_accountlist_id(acc_id):
+def mysql_get_accountlist_name(name):
     conn = get_db_connection()
     cursor = conn.cursor()
 
-    query = "SELECT id,sender_id,receiver_id,amount,tranfer_type,transaction_date,status FROM transfer_list where sender_id like %s or receiver_id like %s order by id desc"
-    cursor.execute(query,(acc_id,))  # 안전한 방식 (SQL Injection 방지)
+    query = "SELECT id,sender_id,receiver_id,(-1)*amount,transaction_type,transaction_date,status,result_balance"
+    query +="FROM transfer_list where sender_id = (select name from accounts where name = %s) and status = 2 and transaction_type = 1"
+    query +="union all"
+    query +="SELECT id,sender_id,receiver_id,amount,transaction_type,transaction_date,status,result_balance"
+    query +="FROM transfer_list where receiver_id = (select name from accounts where name = %s) and status = 2 and transaction_type = 2"
+    query +="order by transaction_date desc;"
+
+    cursor.execute(query,(name,name))  # 안전한 방식 (SQL Injection 방지)
     user = cursor.fetchall()
 
     cursor.close()
