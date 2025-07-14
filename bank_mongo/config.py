a76daa2a4cbb602ff72eadaea5d12833e4f8bb16
minwoo kim <minu0897@naver.com>
2025-07-14 14:38:40 +0900

    del

--- bank_mongo/config.py
+++
@@ -1,7 +0,0 @@
-DB_CONFIG = {
-    "host": "101.235.73.77",
-    "user": "MDBS",
-    "password": "Sejong",
-    "database": "mdbs",
-    "port": "27017"
-}
--- bank_mysql/config.py
+++
@@ -1,7 +0,0 @@
-DB_CONFIG = {
-    "host": "",
-    "port": 33066,
-    "user": "MDBS",
-    "password": "sejong",
-    "database": "mdbs"
-}
